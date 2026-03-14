f002e92195cc77ccf7c1451dfe8098a7fcb68e34
Kreucyu <elciocleiton72@gmail.com>
2026-03-14 19:16:46 -0300

    build: Projeto de serviço de usuários adicionado ao repositório

---
+++ user-service/src/test/java/com/backlog_user_service/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backlog_user_service.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

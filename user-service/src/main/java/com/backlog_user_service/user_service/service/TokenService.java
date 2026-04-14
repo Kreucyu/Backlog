11f93aeace5e2eeb5fca5b8a158c2083902289d2
Kreucyu <elciocleiton72@gmail.com>
2026-04-14 15:58:22 -0300

    feat: Gerador de token no Token Service

--- user-service/src/main/java/com/backlog_user_service/user_service/service/TokenService.java
+++ user-service/src/main/java/com/backlog_user_service/user_service/service/TokenService.java
@@ -1,20 +1,39 @@
 package com.backlog_user_service.user_service.service;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.backlog_user_service.user_service.entity.Usuario;
-import lombok.Value;
-import org.bouncycastle.math.ec.rfc8032.Ed25519;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
 
-    @Value("${api.security.token.secret}");
+
+    @Value("${api.security.token.secret}")
     private String secret;
+
     public String generateToken(Usuario usuario) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("secret");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT
+                    .create()
+                    .withIssuer("auth0")
+                    .withSubject(usuario.getEmailUsuario())
+                    .withExpiresAt(getExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro ao gerar o token", e);
         }
         return null;
+    }
+
+    private Instant getExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }

79e2048f73990324d01bf52a4f6ca60d797182b5
Kreucyu <elciocleiton72@gmail.com>
2026-04-14 16:00:46 -0300

    feat: validador de token

--- user-service/src/main/java/com/backlog_user_service/user_service/service/TokenService.java
+++ user-service/src/main/java/com/backlog_user_service/user_service/service/TokenService.java
@@ -3,6 +3,7 @@ package com.backlog_user_service.user_service.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.backlog_user_service.user_service.entity.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,10 +28,24 @@ public class TokenService {
                     .withSubject(usuario.getEmailUsuario())
                     .withExpiresAt(getExpirationDate())
                     .sign(algorithm);
+            return token;
         } catch (JWTCreationException e) {
             throw new RuntimeException("Erro ao gerar o token", e);
         }
-        return null;
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT
+                    .require(algorithm)
+                    .withIssuer("auth0")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return "";
+        }
     }
 
     private Instant getExpirationDate() {

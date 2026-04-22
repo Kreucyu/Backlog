44ec9a7c94102d337ee8f86d02f64618a37f1f4a
Kreucyu <elciocleiton72@gmail.com>
2026-04-22 14:10:49 -0300

    test: Teste de duplicidade de email concluído

--- user-service/src/test/java/com/backlog_user_service/user_service/service/UsuarioServiceTest.java
+++ user-service/src/test/java/com/backlog_user_service/user_service/service/UsuarioServiceTest.java
@@ -1,6 +1,8 @@
 package com.backlog_user_service.user_service.service;
 
 import com.backlog_user_service.user_service.dto.Request.RegisterUsuarioDto;
+import com.backlog_user_service.user_service.entity.NiveisUsuario;
+import com.backlog_user_service.user_service.entity.Usuario;
 import com.backlog_user_service.user_service.repository.UsuarioRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,13 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class UsuarioServiceTest {
 
@@ -39,6 +42,18 @@ class UsuarioServiceTest {
 
     @Test
     void criarUsuarioEmailJaUtilizado() {
+        String senhaCriptografada = new BCryptPasswordEncoder().encode("Marcio123@");
+        Usuario usuario = new Usuario("Marcio",
+                LocalDate.now(),
+                "MarcioMM@gmail.com",
+                senhaCriptografada,
+                NiveisUsuario.USER);
+        usuarioRepository.save(usuario);
+        when(usuarioRepository.findByEmailUsuario("MarcioMM@gmail.com")).thenReturn(usuario);
+        RegisterUsuarioDto registerUsuarioDto = new RegisterUsuarioDto("Junior", "MarcioMM@gmail.com", "Marcio123@", LocalDate.now());
+        ResponseEntity<String> resposta = usuarioService.criarUsuario(registerUsuarioDto);
+        Assertions.assertEquals("Email já cadastrado", resposta.getBody());
+        verify(usuarioRepository, times(1)).save(any());
     }
 
     @Test
